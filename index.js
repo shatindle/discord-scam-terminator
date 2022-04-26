71f439d742d10ee8a3b5743fbe0a0f66b82362ab
Shane Tindle <sha.tindle@gmail.com>
2022-04-25 21:51:23 -0500

    Added command invoker

--- index.js
+++ index.js
@@ -28,6 +28,21 @@ client.once('ready', async () => {
     await require("./Monitors/nitroSteamScam")(client);
 });
 
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
 // login to client - we should auto reconnect automatically
 client.login(token);
 
