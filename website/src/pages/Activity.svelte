ca001e91ce7e38abb524149c91b52e837f00e315
Shane Tindle <sha.tindle@gmail.com>
2025-01-24 12:53:59 -0600

    Revert "Updated svelte plugins"
    This reverts commit c407a3f0e2584eb912d57044ec762d11cb375f39.

--- website/src/pages/Activity.svelte
+++ website/src/pages/Activity.svelte
@@ -264,7 +264,6 @@
         <div class="row">
             {#each servers.filter(server => server.id.toLowerCase().indexOf(serverFilter.toLowerCase()) > -1 || server.name.toLowerCase().indexOf(serverFilter.toLowerCase()) > -1) as server}
             <!-- svelte-ignore a11y-click-events-have-key-events -->
-            <!-- svelte-ignore a11y_no_static_element_interactions -->
             <div class="col-12 col-sm-6 col-md-4 server-icon {selectedServer === server.id ? "selected" : ""} {server.count === 0 ? "safe" : ""}" on:click={() => setSelectedServer(server.id)}>
                 <div class="row mdc-elevation--z6">
                     <div class="col-3">
